0bcd1ade7fa5dcc998da9f5214bfa7d8329e4eb8
= <pjfritch1@gmail.com>
2017-10-19 11:13:38 -0700

    heaps mostly working

--- parker_fritch_heaps/lib/heap.rb
+++ parker_fritch_heaps/lib/heap.rb
@@ -1,31 +1,110 @@
+require 'byebug'
+
 class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    default_proc = Proc.new do |a,b|
+      a > b
+    end
+    @store = []
+    @prc = prc || default_proc
   end
 
   def count
   end
 
   def extract
+    extract = @store.first
+    last = @store.last
+    @store[0] = last
+    @store[-1] = extract
+    @store.pop
+    @store = BinaryMinHeap.heapify_down(@store, 0, @store.length)
+    extract
   end
 
   def peek
   end
 
   def push(val)
+    @store.push(val)
+    # @store = BinaryMinHeap.heapify_down(@store, 0, @store.length)
+    @store = BinaryMinHeap.heapify_up(@store, (@store.length - 1), @store.length)
+    return val
   end
 
   public
   def self.child_indices(len, parent_index)
+    idx1 = (2 * parent_index + 1)
+    idx2 = (2 * parent_index + 2)
+    indices = []
+    indices << idx1 if idx1 < len
+    indices << idx2 if idx2 < len
+    indices
   end
 
   def self.parent_index(child_index)
+    if child_index == 0
+      raise("root has no parent")
+    else
+      (child_index - 1) / 2
+    end
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc = prc || Proc.new { |a,b| a <=> b}
+    # byebug
+    # p "prc"
+    # p prc
+    # byebug
+    while parent_idx < len
+      children_indices = self.child_indices(len, parent_idx)
+      children = {}
+      children_indices.each do |i|
+        children[array[i]] = i
+      end
+
+      unless children_indices.empty?
+        parent = array[parent_idx]
+        to_swap = children.keys.min
+        swap_idx = children[to_swap]
+        if prc.call(to_swap, parent) < 0
+          if to_swap > parent
+            to_swap = children.keys.max
+            swap_idx = children[to_swap]
+          end
+          swapping = parent
+          array[parent_idx] = to_swap
+          array[swap_idx] = swapping
+        end
+      end
+      parent_idx += 1
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc = prc || Proc.new {|a,b| a <=> b}
+
+    while child_idx > 0
+      parent_index = self.parent_index(child_idx)
+      parent = {}
+      parent[array[parent_index]] = parent_index
+      # p "prc"
+      # p self.prc
+      # unless children_indices.empty?
+        child = array[child_idx]
+        to_swap = parent.keys.first
+        swap_idx = parent[to_swap]
+        if prc.call(child, to_swap) < 0
+          swapping = child
+          array[child_idx] = to_swap
+          array[swap_idx] = swapping
+        end
+      # end
+      child_idx -= 1
+    end
+    array
   end
 end
--- parker_fritch_heaps/lib/heap_sort.rb
+++ parker_fritch_heaps/lib/heap_sort.rb
@@ -2,5 +2,15 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    BinaryMinHeap.heapify_down(self, 0, self.length)
+    p self
+    # BinaryMinHeap.heapify_down(self, 0, self.length)
+    BinaryMinHeap.heapify_up(self, self.length - 1, self.length)
+    p self
   end
+
 end
+
+arr = [5,3,9,1,4,7,8]
+
+arr.heap_sort!.heap_sort!
