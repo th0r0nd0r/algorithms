32a03737c294d0cec8e67972696602445fd9ca51
= <pjfritch1@gmail.com>
2017-10-24 23:12:21 -0700

    blair cache

--- parker_fritch_dynamic_programming/lib/dynamic_programming.rb
+++ parker_fritch_dynamic_programming/lib/dynamic_programming.rb
@@ -1,11 +1,15 @@
 class DynamicProgramming
 
   def initialize
-    @cache = {}
+    @blair_cache = {1=> 1, 2=> 2}
   end
 
   def blair_nums(n)
-
+    return @blair_cache[n] unless @blair_cache[n].nil?
+    n_minus_1th_odd = 1 + (2 * (n - 2))
+    ans = blair_nums(n - 1) + blair_nums(n - 2) + n_minus_1th_odd
+    @blair_cache[n] = ans
+    return ans
   end
 
   def frog_hops_bottom_up(n)
